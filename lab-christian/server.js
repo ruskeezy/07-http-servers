5e128b2e789326d58d9b5826436900d3f4304c9c
Christian Miller <christian.ax.miller@gmail.com>
2018-02-20 20:25:08 -0800

    2 get methods added

--- lab-christian/server.js
+++ lab-christian/server.js
@@ -7,3 +7,34 @@ const cowsay = require('cowsay');
 const parseBody = require('./lib/parse-body.js');
 const PORT = process.env.PORT || 3000;
 
+const server = http.createServer(function(req, res) {
+  req.url = url.parse(req.url);
+  req.url.query = querystring.parse(req.url.query);
+
+  if(req.method === 'GET') {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('hello from my server!');
+    res.end();
+  }
+
+  if(req.method === 'GET' && req.url.pathname === '/cowsay') {
+    let params = req.url.query;
+    if (params.text) {
+      res.writeHead(200, { 'Content-Type': 'text/plain'} );
+      res.write(cowsay.say( { text: params.text }));
+      res.end();
+    }
+    if (!params.text) {
+      res.writeHead(400, { 'Content-Type': 'text/plain' });
+      res.write(cowsay.say( { text: 'bad request' }));
+      res.end();
+    }
+  }
+
+});
+
+server.listen(PORT, () => {
+  console.log(`Server up on PORT ${PORT}`);
+});
+
+
