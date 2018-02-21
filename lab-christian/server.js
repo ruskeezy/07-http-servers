8080e3bdd9fabeceafa258eab3956659cd49353f
Christian Miller <christian.ax.miller@gmail.com>
2018-02-20 19:23:16 -0800

    config files set up, parse-body file done

---
+++ lab-christian/server.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const http = require('http');
+const url = require('url');
+const querystring = require('querystring');
+const cowsay = require('cowsay');
+const parseBody = require('./lib/parse-body.js');
+const PORT = process.env.PORT || 3000;
+
